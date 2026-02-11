dfda9e397a0c2f2d9de33400077084327e2b2d1e
Eren Yeşiltepe <eren.bilkentk15@hotmail.com>
2026-02-11 23:02:32 +0300

    feat: turret pointer

--- jsLibraries/mouseListeners.js
+++ jsLibraries/mouseListeners.js
@@ -14,9 +14,29 @@ function clickListener(e) {
     addBall(plank, localX, localY);
 }
 
+function moveListener(e) {
+    const turret = document.getElementById("turret");
+
+    const gameview = document.getElementById("gameview")
+    const rect = gameview.getBoundingClientRect();
+
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    const turretX = turret.offsetLeft;
+    const turretY = turret.offsetTop;
+
+    const dx = mouseX - turretX;
+    const dy = mouseY - turretY;
+    const angle = Math.atan2(dy, dx) * 180 / Math.PI;
+
+    turret.style.transform = `rotate(${angle}deg)`;
+}
+
 export function bindListeners() {
     const plankElement = document.getElementById("plank");
     plankElement.addEventListener("click", clickListener);
+    plankElement.addEventListener("mousemove", moveListener);
 
     const resetElement = document.getElementById("button");
     resetElement.addEventListener("click", resetState);
